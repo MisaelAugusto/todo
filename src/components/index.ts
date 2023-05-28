277e758accee4f201b1f7a90c8fe90be22d7e1bf
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-05-27 21:42:25 -0300

    test(tests): implemented application tests

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-import Header from "./Header";
-import NewTaskInput from "./NewTaskInput";
-import TodoList from "./TodoList";
-
-export { Header, NewTaskInput, TodoList };
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: true
   },
   resolve: {
     alias: {
-      assets: path.resolve(__dirname, "./src/assets/"),
-      components: path.resolve(__dirname, "./src/components/"),
-    },
-  },
+      assets: path.resolve(__dirname, './src/assets/'),
+      components: path.resolve(__dirname, './src/components/'),
+      mocks: path.resolve(__dirname, './src/mocks/')
+    }
+  }
 });
